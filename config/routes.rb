b78a39c13a87978b23d983cd3e7a6bc2c6a189f6
Caito Scherr <scherr.ca@gmail.com>
2022-05-06 12:42:55 -0700

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "sections#index"
 
+  get "/sections", to: "sections#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
